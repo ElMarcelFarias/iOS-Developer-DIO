3cbf12341f7e78ff437acc3accc4ed95139a3836
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-14 09:39:37 -0300

    New: Protocols e Extensions em andamento

---
+++ dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/protocolos-extensoes.playground/Contents.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+protocol ExempleProtocol {
+    var simplesDescription: String {get}
+    mutating func adjust()
+}
+
+class SimplesClass: ExempleProtocol {
+    var simplesDescription: String = "Uma classe muito simples"
+    var anotherProperty: Int  = 69105
+    func adjust() {
+        simplesDescription += " Agora 100% ajustado"
+    }
+    
+}
+
+
+var a = SimplesClass()
+a.adjust()
+let aDescription = a.simplesDescription
+
+
+struct SimplesStructure: ExempleProtocol {
+    var simplesDescription: String = "Uma struct simples"
+    mutating func adjust() {
+        simplesDescription += "ajustado"
+    }
+}
+
+var b = SimplesStructure()
+b.adjust()
+let bDescription = b.simplesDescription
+
+
+protocol SomeProtocol {
+    init(someParameter: Int)
+}
+
+class SomeClass:SomeProtocol {
+    required init(someParameter: Int) {
+        //Implementacao
+    }
+}
+
+protocol AnotherProtocol {
+    init()
+}
+
+class SomeSuperClass {
+    init() {
+        // implementacao
+    }
+}
+
+
+class SomeSubClass:SomeSuperClass, AnotherProtocol {
+    required override init () {
+        // implementacao
+    }
+}
+
+protocol RandomNumberProtocol {
+    func random() -> Double
+}
+
+class RandomNumber: RandomNumberProtocol {
+    func random() -> Double {
+        return Double.random(in: 0.4...19.2)
+    }
+}
+
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberProtocol
+    
+    init(sides: Int, generetos: RandomNumberProtocol) {
+        self.sides = sides
+        self.generator = generetos
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var random = Dice(sides: 6, generetos: RandomNumber())
+
+for _ in 1...5 {
+    print("O lançamento dos dados aleatórios são: \(random.roll())")
+}
