a68f4ba1f1935fb1fe9de99137313221fc6294ff
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-14 09:02:42 -0300

    Change: revedo async await basico

--- dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/concorrencias-async-await-1.playground/Contents.swift
+++ dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/concorrencias-async-await-1.playground/Contents.swift
@@ -1,34 +1,27 @@
-import UIKit
 import CoreFoundation
 
 /*
 let startTime = CFAbsoluteTimeGetCurrent()
 DispatchQueue.global().sync {
     for i in 0...10 {
-        print("Fui \(i)")
+        print("Fui....\(i)")
     }
 }
+
 
 DispatchQueue.global().sync {
     for i in 0...10 {
-        print("voltei \(i)")
+        print("Voltei....\(i)")
     }
-}
- 
-*/
-
-
-
-/*
-DispatchQueue.main.sync {
-    // Executando na main thread
 }
  */
 
-
-//funcoes assincronas
-
+ //Executando na Main thread
 /*
+DispatchQueue.main.sync {
+    
+}
+ */
 
 func fetchUserId(from server: String) async -> Int {
     if server == "primary" {
@@ -39,49 +32,22 @@ func fetchUserId(from server: String) async -> Int {
 
 
 func fetchUserName(from server: String) async -> String {
-    let userId = await fetchUserId(from: server)
-    if userId == 501 {
-        return "Marcel Leite de Farias"
+    let userID = await fetchUserId(from: server)
+    if userID == 501 {
+        return "Maria clara"
     }
-    return "Convidado"
+    return "Marcel Leite de Farias"
 }
 
-func connectUser(to server: String) async  {
-    
-    async let userId = fetchUserId(from: server)
+func connectUser(to server: String) async {
+    async let userID = fetchUserId(from: server)
     async let userName = fetchUserName(from: server)
-    let greeting = await "Olá \(userName), userId \(userId)"
+    
+    let greeting = await "Olá \(userName), userID \(userID)"
     print(greeting)
 }
-
 
 Task {
     await connectUser(to: "primary")
 }
-
-*/
-
-let gallery = [
-    "Summer Vaction": ["praia.png","campo.png","zoo.png","shopping.png"],
-    "Road Trip": ["paris.png", "roma.png", "bahia.png", "madri.png"]
-]
-
-
-func listPhotos(inGallery name: String) async -> [String] {
-    // Simulacao de rede assincrona
-    do {
-        try await Task.sleep(nanoseconds: 100000)
-    } catch {
-        
-    }
     
-    return gallery[name] ?? []
-}
-
-
-Task {
-    let phoneName = await listPhotos(inGallery: "Summer Vaction")
-    let sortedName = phoneName.sorted()
-    let name = sortedName[0]
-}
-
