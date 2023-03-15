3574b3f1f5316fdaea159ae27ff796007a8dec78
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-15 16:18:11 -0300

    New: finalizado Protocols e Extensions - Tópicos avançados da Linguagem de Programação Swift!

--- dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/protocolos-extensoes.playground/Contents.swift
+++ dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/protocolos-extensoes.playground/Contents.swift
@@ -122,3 +122,128 @@ extension SomeType: ExempleProtocol {
         number += 44
     }
 }
+
+
+var someType = SomeType()
+print(someType.simplesDescription)
+
+extension Double {
+    var km: Double { return self * 1_000.0}
+    var m:  Double { return self }
+    var cm: Double { return self / 100.0}
+    var mm: Double { return self / 1_000.0}
+    var ft: Double { return self / 3.28084}
+}
+
+let oneInch = 25.4.mm
+print("Uma polegada é \(oneInch) metros")
+
+
+let threeFeet = 3.ft
+print("Tres pés sao \(threeFeet) metros")
+
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+
+//duas formas de inicializar um struct
+let defaultRect = Rect()
+let memberWiseReact = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+3.repetitions {
+    print("Hello")
+}
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+
+var someInt = 4
+someInt.square()
+print(someInt)
+
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        return (self / decimalBase) % 10
+    }
+}
+
+
+746381295[0]
+746381295[1]
+746381295[2]
+746381295[8]
+
+
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    
+    var kind: Kind{
+        
+        switch self {
+            case 0:
+                return .zero
+            case let x where x > 0:
+                return .positive
+            default:
+                return .negative
+        }
+        
+    }
+}
+
+
+(10 - 11).kind
+ 
+func printIntegerKind(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case.negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+
+printIntegerKind([3,19, -27, 0, -6, 0, 7])
