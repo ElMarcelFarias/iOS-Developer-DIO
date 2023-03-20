c1cc53800e0454e9f8e1030c1a5f0395716b4171
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-20 15:24:43 -0300

    Change: Primeior exercicio criado para praticar poo em swift

--- dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/propriedades-metodos-iniciadores.playground/Contents.swift
+++ dominando-a-lingugagem-de-programacao-swift/Fundamentos-da-linguagem-de-progracao-swift/propriedades-metodos-iniciadores.playground/Contents.swift
@@ -26,8 +26,34 @@ if(person.status == true) {
 
 
 class Grade {
-    func mediaSemestral (grades: [Int]) -> String {
+    
+    var name: String = ""
+    var grades: [Int] = []
+    var resultGrades: Double = 0.00
+
+    init(name: String, grades: [Int]) {
+        self.name = name
+        self.grades = grades
+    }
+    
+    func averengingSchool () -> String {
+        for grade in grades {
+            resultGrades = resultGrades + Double(grade)
+        }
         
-        return "teste"
+        let aux: Int = grades.count
+        
+        resultGrades = resultGrades / Double(aux)
+        
+        if((resultGrades >= 6) && (resultGrades <= 10)) {
+            return "Media da escola: \(resultGrades) está acima da média"
+            
+        }
+        return "Media da escola: \(resultGrades) está abaixo da média"
     }
 }
+
+var jandiraGrades = Grade(name: "Escola Jandira Davilla", grades: [10,10,10,10])
+var media = jandiraGrades.averengingSchool()
+print("Nome da Escola: \(jandiraGrades.name) \n\(media)")
+
