b6a96a23dea242d75b3e5cd87115bce6c6de8ba5
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-31 16:07:39 -0300

    Change: Finalizando aula Gerenciador de Dependências iOS - Cocoapods!

--- Desenvolvimento de Apps Nativos para iOS/GerenciadorDependencias/GerenciadorDependencias/ViewController.swift
+++ Desenvolvimento de Apps Nativos para iOS/GerenciadorDependencias/GerenciadorDependencias/ViewController.swift
@@ -6,12 +6,70 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    private lazy var chartView: LineChartView = {
+        let lineChartView = LineChartView()
+        lineChartView.backgroundColor = .darkGray
+        lineChartView.rightAxis.enabled = false
+        lineChartView.leftAxis.labelPosition = .outsideChart
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.animate(xAxisDuration: 2.5)
+        return lineChartView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(chartView)
+        
+        print("viewDidLoad")
+        
+        addAnchorConstraint()
+        createChart()
+    }
+    
+    private func addAnchorConstraint() {
+        chartView.translatesAutoresizingMaskIntoConstraints = false
+        chartView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        chartView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        chartView.heightAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    }
+    
+    private func createChart() {
+        
+        let yValues = [
+            ChartDataEntry(x: 0.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 1.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 2.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 3.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 4.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 5.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 6.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 7.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 8.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 9.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 10.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 11.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 12.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 13.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 14.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 15.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 16.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 17.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 18.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 19.0, y: Double.random(in: 1...10)),
+            ChartDataEntry(x: 20.0, y: Double.random(in: 1...10))
+        ]
+        
+        let set1 = LineChartDataSet(entries: yValues, label: "")
+        set1.mode = .cubicBezier
+        set1.drawCirclesEnabled = false
+        
+        let data = LineChartData(dataSet: set1)
+        data.setDrawValues(false)
+        self.chartView.data = data
     }
 
 
