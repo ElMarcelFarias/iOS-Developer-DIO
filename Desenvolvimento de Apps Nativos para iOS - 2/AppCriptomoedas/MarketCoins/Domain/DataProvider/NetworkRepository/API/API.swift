588cdf1ee8ff6341c2088c041ea48154fa8b6f56
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-04-05 18:50:25 -0300

    Change: Programando CoinRouter Routers - Desenvlvimento do App de CriptoMoedas com Swift!

--- Desenvolvimento de Apps Nativos para iOS - 2/AppCriptomoedas/MarketCoins/Domain/DataProvider/NetworkRepository/API/API.swift
+++ Desenvolvimento de Apps Nativos para iOS - 2/AppCriptomoedas/MarketCoins/Domain/DataProvider/NetworkRepository/API/API.swift
@@ -11,7 +11,7 @@ struct API {
     static let baseURL = "https://pro-api.coingecko.com/api/v3/"
     static let coinsMarkets = "/coins/markets"
     static let coinsByIdMarketChart = "/coins/%@/market_chart/range"
-    static let coinsByIdOhlc = "/coins/%$/0hlc"
+    static let coinsByIdOhlc = "/coins/%$/ohlc"
     static let global = "/global"
     static let coinsById = "/coins/%@"
 }
