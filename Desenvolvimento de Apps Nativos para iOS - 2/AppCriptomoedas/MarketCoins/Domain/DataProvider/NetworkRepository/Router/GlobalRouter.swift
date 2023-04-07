fd4b3647807f1803c69fb66af686df7478209568
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-04-07 17:27:51 -0300

    Change: Incluindo Url Request da global Router / NetworkingRepository - Router - GlobalRouter - Desenvolvimento do App de Criptomoedas com Swift !

--- Desenvolvimento de Apps Nativos para iOS - 2/AppCriptomoedas/MarketCoins/Domain/DataProvider/NetworkRepository/Router/GlobalRouter.swift
+++ Desenvolvimento de Apps Nativos para iOS - 2/AppCriptomoedas/MarketCoins/Domain/DataProvider/NetworkRepository/Router/GlobalRouter.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+
+enum GlobalRouter {
+    case global
+    
+    var path: String {
+        switch self {
+        case .global:
+            return API.global
+        }
+    }
+    
+    func asURLRequest() throws -> URL? {
+        guard let url = URL(string: API.baseURL) else {
+            return nil
+        }
+        let urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        switch self {
+        case .global:
+            return urlRequest.url
+        }
+    }
+}
