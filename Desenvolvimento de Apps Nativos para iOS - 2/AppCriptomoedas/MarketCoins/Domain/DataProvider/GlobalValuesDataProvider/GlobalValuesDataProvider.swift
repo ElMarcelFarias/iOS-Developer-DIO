83b5e5b5b816f664f4db0e44af0c5c4125b77613
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-04-10 16:05:02 -0300

    Change: Finalizando toda parte da requisição de API, models, tratativas de dados, Routers, Stores, API e DataProvider!

---
+++ Desenvolvimento de Apps Nativos para iOS - 2/AppCriptomoedas/MarketCoins/Domain/DataProvider/GlobalValuesDataProvider/GlobalValuesDataProvider.swift
@@ -0,0 +1,32 @@
+//
+//  GlobalValuesDataProvider.swift
+//  MarketCoins
+//
+//  Created by Marcel Leite de Farias on 10/04/23.
+//
+
+import Foundation
+
+protocol GlobalValuesDataProviderDelegate: GenericDataProviderDelegate {}
+
+class GlobalValuesDataProvider: DataProviderManager<GlobalValuesDataProviderDelegate, GlobalModel> {
+    
+    private let globalStore: GlobalStoreProtocol?
+    
+    init(globalStore: GlobalStoreProtocol = GloblaStore()) {
+        self.globalStore = globalStore
+    }
+    
+    func fetchGlobalValues() {
+        globalStore?.fetchGlobal(completion: { result, error in
+            if let error = error {
+                self.delegate?.errorData(self.delegate, error: error)
+            }
+            
+            if let result = result{
+                self.delegate?.success(model: result)
+            }
+        })
+    }
+    
+}
